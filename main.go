44664622b2990640c76acad34b8d100726d1d264
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-30 13:18:03 -0500

    Implement main

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var x string
-	var y int
-	var z bool
+	var x string // Default empty => ""
+	var y int    // Default 0
+	var z bool   // Default false
 	fmt.Println(x, " * ", y, " * ", z)
 }
