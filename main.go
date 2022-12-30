6a4e9f9643947e91befa09821098c0f2700cf4d6
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-30 13:21:55 -0500

    Implement main

--- main.go
+++ main.go
@@ -6,5 +6,5 @@ func main() {
 	var x string // Default empty => ""
 	var y int    // Default 0
 	var z bool   // Default false
-	fmt.Println(x, " * ", y, " * ", z)
+	fmt.Printf("%T", x, " * ", y, " * ", z)
 }
