13e832ad2dcef76fbf9926250f1ddc51ad51da26
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-30 13:39:11 -0500

    Implement main

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	var x string = "sina" // Default empty => ""
-	var y bool = true
-	fmt.Printf("%T %T", x, y, "\n")
-	println(x)
+	x := "sina"
+	fmt.Println(x)
 }
