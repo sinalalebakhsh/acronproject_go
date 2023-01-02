05f9bcc75ec8524c362d98cd7218d31d6bbc7ece
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-02 16:51:51 -0500

    Implement main.go

--- main.go
+++ main.go
@@ -6,8 +6,11 @@ import "fmt"
 func main(){
 	var confName = "Go Conf is here"
 
+	const neverChangeID = 1
+
+
 	fmt.Println("Welcome to Acron Project. You can ", confName, ". I like your decision")
-	fmt.Println("Get your Numbers here to attend")
+	fmt.Println("const never change in variable, but can be change in Println:", neverChangeID+100)
 
 	
 
