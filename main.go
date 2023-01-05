e7a6d087aafba9c020770038383cecd445d9615a
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-05 11:02:13 -0500

    Delete All

--- main.go
+++ main.go
@@ -5,21 +5,8 @@ import "fmt"
 
 func main(){
 	
-	var firstName string
-	var lastName string
-	var emailAddress string
-	var phoneNumber string
 
-	fmt.Print("First Name? ")
-	fmt.Scan(&firstName)
-	fmt.Print("Last Name? ")
-	fmt.Scan(&lastName)
-	fmt.Print("Email Address? ")
-	fmt.Scan(&emailAddress)
-	fmt.Print("Phone Number? ")
-	fmt.Scan(&phoneNumber)
-
-	fmt.Printf("Hello %v %v. Your Email is %v, and Phone number is %v\n", firstName, lastName, emailAddress, phoneNumber)
+	
 
 
 
