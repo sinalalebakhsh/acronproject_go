309944c35c2c560b0ebff89a23ed600b34e9e68f
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-05 16:54:54 -0500

    Delete and restructure for better Algoritms

--- main.go
+++ main.go
@@ -10,16 +10,6 @@ func main() {
 	// "For-Each" Loop
 	//  Iterating over a list
 
-	var nameList []string
-	var userInput string
 
-	onlyFirstNames := []string{}
-
-
-	for _, fullNames := range nameList {
-
-		var getFullName = strings.Fields(fullNames)
-		onlyFirstNames = append(onlyFirstNames, getFullName[0])
-	}
-	fmt.Printf("First Name %v\n", onlyFirstNames)
+	
 }
