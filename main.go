9b44d2bad212ad54e717fc018065739666c9dc8b
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-05 15:30:14 -0500

    Append and get userInput and Mix togather

--- main.go
+++ main.go
@@ -7,6 +7,13 @@ func main(){
 	
 	// Slice section
 
+	var nameList [] string
 
+	var userInput string
+	fmt.Print("name? ")
+	fmt.Scan(&userInput)
 
+	nameList = append(nameList, userInput)
+
+	fmt.Printf("nameList: %v\n", nameList)
 }
