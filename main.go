ba631aee3c01dac365fa80130db733fc431c8b8e
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-05 11:38:14 -0500

    Array Option

--- main.go
+++ main.go
@@ -7,10 +7,10 @@ func main(){
 	
 	var person1 = "Lalebakhsh"
 
-	var nameList  [5]string
+	var nameList  [10]string
 
 	nameList[0] = "Sina" + " " + person1
-	nameList[4] = "Miina"
+	nameList[9] = "Miina"
 
 	fmt.Printf("The whole array: %v\n", nameList)
 
