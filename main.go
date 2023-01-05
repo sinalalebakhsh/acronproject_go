6a3a093e03d46b222a06647316d0bc2f29202a69
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-05 15:18:05 -0500

    Delete All for Next Excersice

--- main.go
+++ main.go
@@ -7,26 +7,6 @@ func main(){
 	
 	// Slice section
 
-	var person1 = "Lalebakhsh"
-
-	var nameList  [10]string
-
-	nameList[0] = "Sina" + " " + person1
-	nameList[9] = "Miina"
-
-	fmt.Printf("The whole array: %v\n", nameList)
-
-	fmt.Printf("The first value: %v\n", nameList[0])
-	
-	fmt.Printf("Array type: %T\n", nameList)
-
-	fmt.Printf("Array Length: %v\n", len(nameList))
-
-	
-
-
-
-
 
 
 }
