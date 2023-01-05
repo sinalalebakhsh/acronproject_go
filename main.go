02e61afc9902583fa6d4e0f1655ac554d2b4882a
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-05 16:15:11 -0500

    Implement  Loop

--- main.go
+++ main.go
@@ -10,8 +10,10 @@ func main(){
 	var nameList [] string
 	var userInput string
 
-	
+
 	fmt.Print("Name? ")
 	fmt.Scan(&userInput)
+	nameList = append(nameList, userInput)
+	fmt.Printf("nameList: %v\n", nameList)
 
 }
