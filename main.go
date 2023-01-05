c62468d9fb4204b8c3780a500a9213f367002da9
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-05 16:16:57 -0500

    Implement  Infinite Loops

--- main.go
+++ main.go
@@ -5,15 +5,19 @@ import "fmt"
 
 func main(){
 	
-	// Loops
+	// Infinite Loops
 
 	var nameList [] string
 	var userInput string
 
 
-	fmt.Print("Name? ")
-	fmt.Scan(&userInput)
-	nameList = append(nameList, userInput)
-	fmt.Printf("nameList: %v\n", nameList)
+	
+	for {
+		
+		fmt.Print("Name? ")
+		fmt.Scan(&userInput)
+		nameList = append(nameList, userInput)
+		fmt.Printf("nameList: %v\n", nameList)
+	}
 
 }
