33c00cab2d198c0438f2fb584c7d8298ee8845b8
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-14 06:00:42 -0500

    Review var listFullName []string

--- main.go
+++ main.go
@@ -9,17 +9,21 @@ func main() {
 	// "For-Each" Loop
 	//  Iterating over a list
 
-var firstName string
-fmt.Print("What's You First Name? ")
-fmt.Scan(&firstName)
+	var firstName string
+	fmt.Print("What's You First Name? ")
+	fmt.Scan(&firstName)
 
-var lastName string
-fmt.Print("What's You Last Name? ")
-fmt.Scan(&lastName)
+	var lastName string
+	fmt.Print("What's You Last Name? ")
+	fmt.Scan(&lastName)
 
+	fmt.Printf("Hello %v %v\n", firstName, lastName)
+	fmt.Printf("First name memory address: %v\n", &firstName)
+	fmt.Printf("Last name memory address: %v\n", &lastName)
 
-fmt.Printf("Hello %v %v\n", firstName, lastName)
-fmt.Printf("First name memory address: %v\n", &firstName)
-fmt.Printf("Last name memory address: %v\n", &lastName)
-	
+	var listFullName []string
+	listFullName = append(listFullName, firstName + " " + lastName)
+
+	fmt.Printf("List Full Name: %v\n", listFullName)
+
 }
