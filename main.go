d9ed05af9f676bee422fe254fccfda723a6b9152
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-14 02:39:25 -0500

    Reveiw Get first name and last name

--- main.go
+++ main.go
@@ -9,10 +9,17 @@ func main() {
 	// "For-Each" Loop
 	//  Iterating over a list
 
-var userInput string
-fmt.Print("What's You Name? ")
-fmt.Scan(&userInput)
-fmt.Printf("Hello %v\n", userInput)
-fmt.Printf("Your name have memory address: %v\n", &userInput)
+var firstName string
+fmt.Print("What's You First Name? ")
+fmt.Scan(&firstName)
+
+var lastName string
+fmt.Print("What's You Last Name? ")
+fmt.Scan(&lastName)
+
+
+fmt.Printf("Hello %v %v\n", firstName, lastName)
+fmt.Printf("First name memory address: %v\n", &firstName)
+fmt.Printf("Last name memory address: %v\n", &lastName)
 	
 }
