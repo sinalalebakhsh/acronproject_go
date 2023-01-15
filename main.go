e7f2a80e7afb3f2f01b9a861408459ae79ab0059
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-15 13:19:41 -0500

    Implement ++

--- main.go
+++ main.go
@@ -3,23 +3,32 @@ package main
 
 import (
 	"fmt"
+	// "strings"
 )
 
 func main() {
 
 	fmt.Println("Welcome to this Program ...[*]")
 
-	for {
-		//===============================================================================================
-		var firstName string
-		fmt.Print("What's You First Name? ")
-		fmt.Scan(&firstName)		
-		isValidUserName := len(firstName) >= 3
+	//===============================================================================================
+	for{
+	var firstName string
+	fmt.Print("What's You First Name? ")
+	fmt.Scan(&firstName)		
+	isValidUserName := len(firstName) >= 3
 		if isValidUserName{
+			fmt.Printf("Hello %v\n", firstName)
 			break
 		}
-		
-
-
 	}
+	// for {
+	// 	var emailAddress string
+	// 	fmt.Print("What's You First Name? ")
+	// 	fmt.Scan(&emailAddress)		
+	// 	isValidEmail := strings.Contains(emailAddress, "@")
+	// 	if isValidEmail{
+	// 		break
+	// 	}
+	// }
+	
 }
