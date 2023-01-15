d461a7a76c0d1dfeb659acd200bd29dc7a8014c0
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-15 06:13:53 -0500

    Implement and Add Pointer

--- main.go
+++ main.go
@@ -11,28 +11,55 @@ func main() {
 	var firstName string
 	fmt.Print("What's You First Name? ")
 	fmt.Scan(&firstName)		
-	
+	Counter := 0
+
 	for {
-		var userTickets int
-		fmt.Printf("Hello %v, How many Tickets number? ", firstName)
-		fmt.Scan(&userTickets)		
-
-		if AllTickets > userTickets {
-			AllTickets = AllTickets - userTickets
-		}else if AllTickets == userTickets {
-			fmt.Printf("Oh!! You get every tickets! Thank for buying ... [*]\n")
-			break
-		}else{
-			fmt.Print("Teckets Number more than All teckets ...[*] \n")
-			break
-		}
 		
-
-
-		if AllTickets == 0 {
-			// End Loop:
-			fmt.Printf("You Get %v Teckets, So is Finished...[*]\n ", userTickets)
-			break
+		if Counter == 0 {
+			Counter = 1
+			var userTickets int
+			fmt.Printf("Hello %v, How many Tickets number? ", firstName)
+			fmt.Scan(&userTickets)		
+	
+			if AllTickets > userTickets {
+				AllTickets = AllTickets - userTickets
+			}else if AllTickets == userTickets {
+				fmt.Printf("Oh!! You get every tickets! Thank for buying ... [*]\n")
+				break
+			}else{
+				fmt.Print("Teckets Number more than All teckets ...[*] \n")
+				break
+			}
+			
+	
+	
+			if AllTickets == 0 {
+				// End Loop:
+				fmt.Printf("You Get %v Teckets, So is Finished...[*]\n ", userTickets)
+				break
+			}
+		}else {
+			var userTickets int
+			fmt.Print("How many Tickets number? ")
+			fmt.Scan(&userTickets)		
+	
+			if AllTickets > userTickets {
+				AllTickets = AllTickets - userTickets
+			}else if AllTickets == userTickets {
+				fmt.Printf("Oh!! You get every tickets! Thank for buying ... [*]\n")
+				break
+			}else{
+				fmt.Print("Teckets Number more than All teckets ...[*] \n")
+				break
+			}
+			
+	
+	
+			if AllTickets == 0 {
+				// End Loop:
+				fmt.Printf("You Get %v Teckets, So is Finished...[*]\n ", userTickets)
+				break
+			}
 		}
 		
 	}
