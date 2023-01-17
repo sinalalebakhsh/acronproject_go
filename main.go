b8c5c075a98995d329d7725fc1f4e06a5f74ff63
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-17 17:22:23 -0500

    Return from user input three variables

--- main.go
+++ main.go
@@ -10,11 +10,7 @@ func main() {
 	//===============================================================================================
 	Get_User_Input()	
 	//===============================================================================================
-	firstValue, secondValue, thirdValue := Returning_Multiple_values()
-	fmt.Println("     ")
-	fmt.Println("    **************************** ")
-	fmt.Printf("%v   %v   %v\n", firstValue, secondValue, thirdValue)
-	fmt.Println("    **************************** ")
+
 	//===============================================================================================
 	Goodbye()
 }
@@ -24,7 +20,7 @@ func Wellcome()  {
 	fmt.Println("Welcome to this Program ...[*]")	
 }
 
-func Get_User_Input()  {
+func Get_User_Input() (string, string) {
 	
 	for{
 		var firstName string
@@ -36,20 +32,20 @@ func Get_User_Input()  {
 		if isValidUserName{
 				fmt.Print("What's Your City Name? ")
 				fmt.Scan(&userCity)		
-				switch userCity {
-				case "Tehran":
-					fmt.Printf("%v! Your city is Capital of the Iran. \n", firstName)
-				case "Gilan":
-					fmt.Printf("%v! Your city is in north of the Iran. \n", firstName)
-				case "New-York", "Washington":
-					fmt.Printf("%v! Your city is in the United States of America. \n", firstName)
-				default:
-					fmt.Printf("I dont know where is it %v\n", userCity)
-				}
-				break
+				return firstName, userCity
 			}
 		}
 }
+// switch userCity {
+// case "Tehran":
+// 	fmt.Printf("%v! Your city is Capital of the Iran. \n", firstName)
+// case "Gilan":
+// 	fmt.Printf("%v! Your city is in north of the Iran. \n", firstName)
+// case "New-York", "Washington":
+// 	fmt.Printf("%v! Your city is in the United States of America. \n", firstName)
+// default:
+// 	fmt.Printf("I dont know where is it %v\n", userCity)
+// }
 // ==================================================================
 // func Returning_Multiple_values(Input) (Out put Finction OR returned)
 // ==================================================================
