7ecbe5be824038ca810edcc983d25890ee9d1033
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-19 15:16:57 -0500

    Implement helper.go

---
+++ helper.go
@@ -0,0 +1,10 @@
+package main
+
+func get_Informations() (string, string){
+
+	get_first_name := get_first_name()
+
+	get_country_name := get_country_name()
+
+	return get_first_name, get_country_name
+}
