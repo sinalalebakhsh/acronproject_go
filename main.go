bc1003d2101855e0977bba933f4bb2630a97fdcb
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-19 05:31:44 -0500

    Go Compact Code

--- main.go
+++ main.go
@@ -1,50 +1,66 @@
-// Returning Multiple values from a function	
-package main
+//Go Compact Code
+package main; import ("fmt"); func main() { fmt.Println("Hello World!");}
 
-import (
-	"fmt"
-)
+// // Returning Multiple values from a function	
+// package main
 
-func main() {
-	Wellcome()	
-	// ===============================================================================================
-	get_Informations()	
-	// ===============================================================================================	
-	// ===============================================================================================
-	Goodbye()
-}
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	Wellcome()	
+// 	// ===============================================================================================
+// 	get_Informations()	
+// 	// ===============================================================================================	
+// 	// ===============================================================================================
+// 	Goodbye()
+// }
 
 
-func Wellcome()  {
-	fmt.Println("Welcome to this Program ...[*]")	
-}
+// func Wellcome()  {
+// 	fmt.Println("Welcome to this Program ...[*]")	
+// }
 
-func get_Informations()  {
+// func get_Informations()  {
 
-	var firstName string
-	fmt.Print("What's You First Name? ")
-	fmt.Scan(&firstName)
-	for {
-		if len(firstName) <= 3{
-			fmt.Print("Pleas write name more than 3 caracters, What's You First Name? ")
-			fmt.Scan(&firstName)	
-		}else if len(firstName) > 3 {
-			break
-		}
-	}
+// 	var firstName string
+// 	fmt.Print("What's Your First Name? ")
+// 	fmt.Scan(&firstName)
+// 	for {
+// 		if len(firstName) <= 3{
+// 			fmt.Print("Pleas write name more than 3 caracters, What's Your First Name? ")
+// 			fmt.Scan(&firstName)	
+// 		}else if len(firstName) > 3 {
+// 			break
+// 		}
+// 	}
+
+// 	var country_name string
+// 	fmt.Print("What's You Country Name? ")
+// 	fmt.Scan(&country_name)
+// 	for {
+// 		if len(country_name) <= 4{
+// 			fmt.Print("Pleas write name more than 4 caracters, What's Your Coutry Name? ")
+// 			fmt.Scan(&country_name)	
+// 		}else if len(country_name) > 4 {
+// 			break
+// 		}
+// 	}
+
+// 	fmt.Printf("Your first name = %v\n Your Country = %v\n", firstName, country_name)
 	
-	
-}
+// }
 
-func Goodbye()  {
-	fmt.Println("   ")
-	fmt.Println("   ")
-	fmt.Println(" [*]----------------------[*]  ")
-	fmt.Println(" [*]----THANKS.GOODBYE----[*]  ")
-	fmt.Println(" [*]----------------------[*]  ")
-	fmt.Println("   ")
-	fmt.Println("   ")
-}
+// func Goodbye()  {
+// 	fmt.Println("   ")
+// 	fmt.Println("   ")
+// 	fmt.Println(" [*]----------------------[*]  ")
+// 	fmt.Println(" [*]----THANKS.GOODBYE----[*]  ")
+// 	fmt.Println(" [*]----------------------[*]  ")
+// 	fmt.Println("   ")
+// 	fmt.Println("   ")
+// }
 
 
 
