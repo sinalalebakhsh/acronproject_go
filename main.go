a775f17a9b4c1583654a6f807dc57da60c435dd7
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-19 04:45:56 -0500

    Delete All unuseful

--- main.go
+++ main.go
@@ -8,9 +8,8 @@ import (
 func main() {
 	Wellcome()	
 	// ===============================================================================================
-	firstName, userCountry, userCity := get_user_Input()	
-	// ===============================================================================================
-	use_user_input(firstName, userCountry, userCity)
+	get_user_Input()	
+	// ===============================================================================================	
 	// ===============================================================================================
 	Goodbye()
 }
@@ -20,57 +19,16 @@ func Wellcome()  {
 	fmt.Println("Welcome to this Program ...[*]")	
 }
 
-func get_user_Input() (string, string, string) {
-	for{
-		var firstName string
-		var userCountry string
-		var userCity string
+func get_user_Input()  {
 
-		fmt.Print("What's You First Name? ")
-		fmt.Scan(&firstName)
-		isValidUserName := len(firstName) >= 3
+	var firstName string
+	
+	fmt.Print("What's You First Name? ")
+	fmt.Scan(&firstName)
 
-		fmt.Print("What's Your Country Name? ")
-		fmt.Scan(&userCountry)		
-		isValidUserCountry := len(userCountry) >= 4
-		
-		fmt.Print("What's Yout City Name? ")
-		fmt.Scan(&userCity)
-		isValidUserCity := len(userCity) >= 4
-
-		if isValidUserName && isValidUserCountry && isValidUserCity{
-				return firstName, userCountry, userCity
-			}
-		}		
-}
-
-// ==================================================================
-// func Returning_Multiple_values(Input) (Out put Finction OR returned)
-// ==================================================================
-
-func use_user_input(firstName string, userCountry string, userCity string)  {
-	fmt.Println("   ")
-	fmt.Printf(" Hello %v\n  ", firstName)
-	fmt.Println("   ")
-
-	switch userCountry{
-	case "Iran":
-		fmt.Println("Iran is in continent of Asia.")
-	case "America":
-		fmt.Println("America is in continent of North America.")
-	default:
-		fmt.Printf("For now I don't know where is it %v\n.", userCountry)
-	}
-
-	switch userCity {
-	case "Tehran":
-		fmt.Printf("%v! Your city is Capital of the Iran. \n", firstName)
-	case "Gilan":
-		fmt.Printf("%v! Your city is in north of the Iran. \n", firstName)
-	case "New-York", "Washington":
-		fmt.Printf("%v! Your city is in the United States of America. \n", firstName)
-	default:
-		fmt.Printf("I dont know where is it %v\n", userCity)
+	for len(firstName) <= 3 {
+		fmt.Print("Pleas write name more than 3 caracters, What's You First Name? ")
+		fmt.Scan(&firstName)	
 	}
 	
 }
