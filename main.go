ca06ef4f6fdc7cfd44fa591914720e21818535ec
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-19 06:29:38 -0500

    Debugging

--- main.go
+++ main.go
@@ -1,28 +1,26 @@
-// Returning Multiple values from a function	
+// Returning Multiple values from a function
 package main
 
 import (
-	"fmt"
 	"bufio"
+	"fmt"
 	"os"
 )
 
 func main() {
-	Wellcome()	
+	Wellcome()
 	// ===============================================================================================
-	get_Informations()	
-	// ===============================================================================================	
+	get_Informations()
+	// ===============================================================================================
 	// ===============================================================================================
 	Goodbye()
 }
 
-
-func Wellcome()  {
-	fmt.Println("Welcome to this Program ...[*]")	
+func Wellcome() {
+	fmt.Println("Welcome to this Program ...[*]")
 }
 
-func get_Informations()  {
-
+func get_Informations() {
 
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter First-Name: ")
@@ -31,40 +29,39 @@ func get_Informations()  {
 		panic(err) // Don't forget to check and handle returned errors!
 	}
 
-	if firstName == "\n" {
-		for{
-			fmt.Print("Enter firstName again: ")
-			firstName, err := reader.ReadString('\n')
-			if err != nil {
-				panic(err) // Don't forget to check and handle returned errors!
-			}
-			if firstName != "\n"{
-				break
-			}
+	for len(firstName) < 5 {
+		fmt.Print("First-Name must be more than 3 characters: ")
+		firstName, err := reader.ReadString('\n')
+		if err != nil {
+			panic(err) // Don't forget to check and handle returned errors!
 		}
-	} else {
-		fmt.Println("Hello", text)
+		if len(firstName) > 4 {
+			fmt.Scan(&firstName) 
+			break
+		}
 	}
+
+	fmt.Println("Hello", firstName)
+
 	// var firstName string
 	// fmt.Print("What's Your First Name? ")
 	// fmt.Scan(&firstName)
-	
+
 	// if firstName == "\n"{
 	// 	for {
 	// 		fmt.Print("Pleas write name more than 3 caracters, What's Your First Name? ")
-	// 		fmt.Scan(&firstName)	
+	// 		fmt.Scan(&firstName)
 	// 		if len(firstName) > 3 {
 	// 			break
 	// 		}
 	// 	}
 	// }
 
-
 	// fmt.Printf("Your first name = %v\n", firstName)
-	
+
 }
 
-func Goodbye()  {
+func Goodbye() {
 	fmt.Println("   ")
 	fmt.Println("   ")
 	fmt.Println(" [*]----------------------[*]  ")
@@ -73,13 +70,3 @@ func Goodbye()  {
 	fmt.Println("   ")
 	fmt.Println("   ")
 }
-
-
-
-
-
-
-
-
-
-
