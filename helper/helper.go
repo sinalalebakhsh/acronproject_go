0b1796d08f0265fd4c1f83095131334322ffaa66
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-22 18:07:18 -0500

    Create New funstion + 	user_Data[age_number] = strconv.FormatUint(uint64(get_age_number), 10)
    Create the other Map

--- helper/helper.go
+++ helper/helper.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func Get_Informations() (string, string){
@@ -17,7 +18,7 @@ func Get_Informations() (string, string){
 	var user_Data = make(map[string]string)
 	user_Data["first_name"] = get_first_name
 	user_Data["country_name"] = get_country_name
-	
+	user_Data["age_number"] = strconv.FormatUint(uint64(get_age_number), 10)
 
 	return get_first_name, get_country_name
 }
