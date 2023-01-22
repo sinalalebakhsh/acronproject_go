61861bedab13a5776f27c6605bf51436abbb2886
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-22 05:49:25 -0500

    Debugging rename in import = package helper

---
+++ helper/helper.go
@@ -0,0 +1,58 @@
+package helper
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func Get_Informations() (string, string){
+
+	get_first_name := Get_first_name()
+
+	get_country_name := Get_country_name()
+
+	return get_first_name, get_country_name
+}
+
+func Get_first_name() (string) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter First-Name: ")
+	firstName, _ := reader.ReadString('\n')	
+	if len(firstName) >= 5 {
+		return firstName
+	}
+	for {
+		fmt.Print("First-Name must be more than 3 characters: ")	
+		firstName, _ := reader.ReadString('\n')	
+		if len(firstName) < 5 {
+			continue
+		}else if len(firstName) >=5{
+			return firstName
+		} 
+
+	}
+}
+
+func Get_country_name() (string) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter Country-Name: ")
+	country_name, _ := reader.ReadString('\n')	
+	if len(country_name) >= 5 {
+		return country_name
+	}
+	for {
+		fmt.Print("Country-Name must be more than 4 characters: ")	
+		country_name, _ := reader.ReadString('\n')	
+		if len(country_name) < 5 {
+			continue
+		}else if len(country_name) >= 5 {
+			return country_name
+		} 
+
+	}
+}
+
+func Print(userinput string){
+	fmt.Println(userinput)
+}
