1ce24e2859776bac0a0a210aecf6936d4fcca91c
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-23 03:11:55 -0500

    Delete testing validation & invalidation

--- main.go
+++ main.go
@@ -7,20 +7,9 @@ import "acronproject/helper"
 
 
 func main() {
-	// helper.Get_Informations()
-	// helper.Goodbye()
 
-	var user_input = 30
-	var user_ai = 20
-
-	isValid := user_ai > user_input
-
-	if isValid{
-		helper.Print("Is valid")
-	} else if !isValid {
-		helper.Print("Is invalid")
-	}
-
+	helper.Get_Informations()
+	helper.Goodbye()
 
 }
 
