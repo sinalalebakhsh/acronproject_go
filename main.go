26cecabb1538026e1823ab043a9b73efbaa579c8
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-22 19:12:40 -0500

    Debugging +

--- main.go
+++ main.go
@@ -6,15 +6,13 @@ import (
 	"fmt"
 )
 
-var global_variable = "Sina Lalebakhsh...[*]"
 
 func main() {
 
 	helper.Print("Salam Sina")
 
-	first_name, country_name := helper.Get_Informations()
+	helper.Get_Informations()
 
-	fmt.Printf("%v\n%v\n", first_name, country_name)
 	Goodbye()
 }
 
