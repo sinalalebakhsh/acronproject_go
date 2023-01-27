e30ed82ba850315318b6480aa315bed50680d8de
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-27 13:17:45 -0500

    Implement main.go

--- main.go
+++ main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"github.com/sinalalebakhsh/acronproject_go/data"
-	"github.com/jalaali/go-jalaali"
 )
 
 func main()  {
-	fmt.Println(time.Now().Date())
-	fmt.Println(jalaali.ToJalaali(time.Now().Date()))
 	fmt.Println(data.Name)
+	data.PrintNowDate()
 }
 
