65edd1873b9e41cfd908f46996934f629ddfc21f
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-27 13:17:26 -0500

    date.go

--- data/date.go
+++ data/date.go
@@ -1,3 +1,14 @@
 package data
 
+import (
+	"fmt"
+	"time"
+	"github.com/jalaali/go-jalaali"
+)
+
 var Name string = "Sina"
+
+func PrintNowDate()  {
+	fmt.Println(time.Now().Date())
+	fmt.Println(jalaali.ToJalaali(time.Now().Date()))
+}
