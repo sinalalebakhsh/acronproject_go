d84f60a773e74fb90659636c2bb2e865cbe28362
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-28 03:44:09 -0500

    Delete All for the other learning

--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/sinalalebakhsh/acronproject_go
-
-go 1.18
-
-require github.com/jalaali/go-jalaali v0.0.0-20210801064154-80525e88d958
--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"github.com/sinalalebakhsh/acronproject_go/data"
+
 )
 
 func main()  {
-	fmt.Println(data.Name)
-	data.PrintNowDate()
+
 }
 
