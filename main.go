d86be40d1bff5401d7f28ddfd68695fab236c9cc
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-28 13:11:08 -0500

    main.go Imp

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/sinalalebakhsh/acronproject_go/doctor"
 )
 
 func main()  {
 	
+	reader := bufio.NewReader()
 
 	whatToSay := doctor.Intro()
 
