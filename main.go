fbe99a6ee84f0c69c1f44a5b07da89b67e1ce2dd
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-28 10:36:42 -0500

    Intro Imp

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
-
+	"fmt"
 )
 
 func main()  {
+	fmt.Println(Intro())
+}
 
+
+func Intro() string  {
+	return `
+Hello
+| I'm Sina.
+| Is the other programming I wrote it.
+| like Go language.
+| my goal is writting security tools for easier penetration web-applications
+| Linkedin profile is Sinalalebakhsh.
+| to my Target.
+	
+	`
 }
 
