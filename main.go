b5d694b26f2eff021897aff184c7a29cf671d7e2
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-28 13:02:00 -0500

    main.go Imp

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/sinalalebakhsh/acronproject_go/doctor"
 )
 
 func main()  {
-	
+	var whatToSay string
+
+	whatToSay = doctor.Intro()
+
+	fmt.Println(whatToSay)
 }
 
 
