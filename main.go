bad86251a7f29b297125b436f0df2ad4220aec90
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-29 08:01:50 -0500

    main.go Imp

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 )
 
+const prompt = "and press ENTER when ready"
+
 func main()  {
 	// Guess the Number Game
 	// Answer to this question:    What I need to do in this program ?
@@ -14,16 +16,16 @@ func main()  {
 	// 3- give them the answer
 	//-------------------------------------------------------------------------
 	var	firstNumber = 2 
-	// var secondNumber = 5
-	// var subtraction = 7
-	// var answer int
+	var secondNumber = 5
+	var subtraction = 7
+	var answer int
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("")
 	fmt.Println("	||==========================||")
 	fmt.Println("	||  Guess the Number Game   ||")
 	fmt.Println("	||==========================||")
 	fmt.Println("")	
-	fmt.Println("Think of a number between 1 and 10 and press ENTER when ready: ")
+	fmt.Println("Think of a number between 1 and 10")
 	reader.ReadString('\n')
 
 	// 2- take them through the games
