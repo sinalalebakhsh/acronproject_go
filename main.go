608959ac373a99617096ff3753a45bb77301c79b
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-29 07:48:24 -0500

    main.go Imp

--- main.go
+++ main.go
@@ -12,22 +12,22 @@ func main()  {
 	// 1- Display a wellcome/instructions 
 	// 2- take them through the games
 	// 3- give them the answer
-	// var	firstNumber = 2 
-	// var secondNumber = 5
-	// var subtraction = 7
-	// var answer int
-
+	//-------------------------------------------------------------------------
+	var	firstNumber = 2 
+	var secondNumber = 5
+	var subtraction = 7
+	var answer int
 	reader := bufio.NewReader(os.Stdin)
-
 	fmt.Println("")
 	fmt.Println("	||==========================||")
 	fmt.Println("	||  Guess the Number Game   ||")
 	fmt.Println("	||==========================||")
-	fmt.Println("")
-
+	fmt.Println("")	
 	fmt.Println("Think of a number between 1 and 10 and press ENTER when ready: ")
 	reader.ReadString('\n')
 
+	// 2- take them through the games
+	
 
 }
 
