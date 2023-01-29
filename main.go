ba593f3b62cc19a3c637721c335cff5a901fb2c3
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-29 05:16:28 -0500

    Delete All

--- main.go
+++ main.go
@@ -1,38 +1,11 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-
-	"github.com/sinalalebakhsh/acronproject_go/doctor"
+	
 )
 
 func main()  {
 	
-	reader := bufio.NewReader(os.Stdin)
-
-	whatToSay := doctor.Intro()
-
-	fmt.Println(whatToSay)
-
-	for  {
-		fmt.Print("-> ")
-		userInput, _ := reader.ReadString('\n')
-	
-		userInput = strings.Replace(userInput, "\r\n", "", -1)
-		userInput = strings.Replace(userInput, "\n", "", -1)
-
-
-		if userInput == "quit"  {
-			break
-			} else  {
-			fmt.Println(doctor.Response(userInput))
-		}
-
-
-	}
 }
 
 
