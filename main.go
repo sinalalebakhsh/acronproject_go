07ccf19bd7f59cc4fccf3cab46d653b44ba360d8
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-29 07:36:10 -0500

    main.go Imp

--- main.go
+++ main.go
@@ -4,24 +4,27 @@ import "fmt"
 
 func main()  {
 	// Guess the Number Game
+	// Answer to this question:    What I need to do in this program ?
+	// 1- Display a wellcome/instructions 
+	// 2- take them through the games
+	
+	// 3- give them the answer
 	var	firstNumber = 2 
 	var secondNumber = 5
 	var subtraction = 7
 	var answer int
 
-	// Answer to this question:    What I need to do in this program ?
+	
 
-	// 1- Display a wellcome/instructions 
 	fmt.Println("")
 	fmt.Println("	||==========================||")
 	fmt.Println("	||  Guess the Number Game   ||")
 	fmt.Println("	||==========================||")
 	fmt.Println("")
-fmt.Println("Think of a number between 1 and 10 and press ENTER when ready: ")
 
-	// 2- take them through the games
+	fmt.Println("Think of a number between 1 and 10 and press ENTER when ready: ")
 
-	// 3- give them the answer
+
 
 }
 
