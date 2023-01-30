a9e3dcdb5975ca3b88ce098df2ebf63c03f13992
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-30 09:38:31 -0500

    main.go Imp

--- main.go
+++ main.go
@@ -7,6 +7,15 @@ import(
 
 
 func main()  {
+// variables
+// declare a package level variable for the main
+// package name myVar
+
+// declare a block variable for the main function
+// called blockVar
+
+
+
 }
 
 
