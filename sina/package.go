db3c6ce0c6cb2df4d8393911e09c1bd097b25afb
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-30 13:55:07 -0500

    Renamed ...

--- sina/package.go
+++ sina/package.go
@@ -2,9 +2,6 @@ package packageone
 
 import "fmt"
 
-
-
-
 // declare a package level variable in the packageone
 // package named PackageVar
 var PackageVar = "PackageVar" 
@@ -12,6 +9,6 @@ var PackageVar = "PackageVar"
 
 
 // create an exported function in package called PrintMe
-func PrintMe()  {
-	fmt.Println("PrintMe() func")
+func PrintMe(s1, s2 string)  {
+	fmt.Println(s1, s2, PackageVar)
 }
