fd339d612f5d9da9b98e170aa7f8218925f98d71
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-30 13:34:58 -0500

    Impt packageone.go

--- package/package.go
+++ package/package.go
@@ -2,14 +2,16 @@ package packageone
 
 import "fmt"
 
+
+
+
 // declare a package level variable in the packageone
 // package named PackageVar
-// create an exported function in package called PrintMe
-
-
-
 var PackageVar = "PackageVar" 
 
+
+
+// create an exported function in package called PrintMe
 func PrintMe()  {
 	fmt.Println("PrintMe() func")
 }
