d63d4afa3421d99af690dc8985eb85107e02a9fb
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-30 09:36:38 -0500

    main.go Imp

--- main.go
+++ main.go
@@ -1,23 +1,13 @@
 package main
 
 import(
-	"fmt"
+
 )
 
-var one = " Is global access One"
+
 
 func main()  {
-	var one = "One"
-	fmt.Println(one)
-
-	// What do you think will happen ?
-	MyFunc() 
 }
 
-
-func MyFunc()  {
-	
-	fmt.Println(one)
-}
 
 
