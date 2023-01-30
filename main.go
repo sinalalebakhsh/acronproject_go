82dd06e4105f886323c62dff20f8e78d3ab01d4a
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-30 13:30:59 -0500

    main.go Imp

--- main.go
+++ main.go
@@ -1,37 +1,24 @@
 package main
 
-import "fmt"
-
+import (
+	"fmt"
+	"acronproject/package"
+)
 
 // declare a package level variable for the main
 // package name myVar
 var myVar = "Package level variable"
 
-func main()  {
+func main() {
 	// variables
-
 
 	// declare a block variable for the main function
 	// called blockVar
-	blockVar := "blockVar"
-	fmt.Println(blockVar)
 
-
-	// declare a package level variable in the package
-	// package named PackageVar
-
-	{
-		PackageVar := "PackageVar" 
-		fmt.Println(PackageVar)
-	}
-
-	// create an exported function in package called PrintMe
 
 	// in the main function, print out the values of myVar,
 	fmt.Println(myVar)
 	// blockVar, and PackageVar on one line
 	//function in packageone
+	packageone.PrintMe()
 }
-
-
-
