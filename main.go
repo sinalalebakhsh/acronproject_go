66dac0c767f19a0d28e295359efbfe51e7ec9c16
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-30 05:49:03 -0500

    main.go Imp

--- main.go
+++ main.go
@@ -21,24 +21,47 @@ func Introduction ()  {
 
 
 
+const prompt = "and don't type you number here, just press ENTER when ready"
+
 func main()  {
 	Wellcome() 
 	//--------------------------------------------------------------------------------------------------
 	
-	const prompt = "and don't type you number here, just press ENTER when ready"
 	rand.Seed(time.Now().UnixNano())
 	//-------------------------------------------------------------------------
 	var	firstNumber = rand.Intn(4) + 2 
 	var secondNumber = rand.Intn(4) + 2
 	var subtraction = rand.Intn(4) + 2
-	reader := bufio.NewReader(os.Stdin)
 	var answer = firstNumber * secondNumber - subtraction
 	
-
-
+	PlayTheGame(firstNumber, secondNumber, subtraction, answer)
+	
 	//--------------------------------------------------------------------------------------------------
 	GoodBye()
 	Introduction()
+}
+
+func PlayTheGame(firstNumber, secondNumber, subtraction, answer int) {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Think of a number between 1 and 10", prompt)
+	reader.ReadString('\n')
+	
+	// 2- take them through the games
+	fmt.Println("Multiply your number by", firstNumber, prompt)
+	reader.ReadString('\n')
+	
+	fmt.Println("Now multiply the result by", secondNumber, prompt)
+	reader.ReadString('\n')
+	
+	fmt.Println("Divide the result by the number you originally thought of", prompt)
+	reader.ReadString('\n')
+	
+	fmt.Println("Now subtract", subtraction, prompt)
+	reader.ReadString('\n')
+	// 3- give them the answer
+	fmt.Println("The answer is", answer)
+	
 }
 
 func Wellcome()  {
@@ -55,9 +78,6 @@ func GoodBye()  {
 	fmt.Println("	||         GoodBye          ||")
 	fmt.Println("	||==========================||")
 	fmt.Println("")	
-}
-func PlayTheGame(firstNumber, secondNumber, subtraction, answer int) {
-
 }
 // Finishing Guess of the Number Game
 
