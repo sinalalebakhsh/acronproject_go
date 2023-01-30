3c70ad59f07e22b12fb0b7ce3dc2a1400691b7de
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-01-30 13:04:18 -0500

    main.go Imp

--- main.go
+++ main.go
@@ -1,22 +1,27 @@
 package main
 
-import(
-
-)
+import "fmt"
 
 
+var myVar = "myVar"
 
 func main()  {
 // variables
 // declare a package level variable for the main
 // package name myVar
 
+
 // declare a block variable for the main function
 // called blockVar
-
+blockVar := "blockVar"
+fmt.Println(blockVar)
 // declare a package level variable in the package
 // package named PackageVar
 
+{
+	PackageVar := "PackageVar" 
+	fmt.Println(PackageVar)
+}
 
 // create an exported function in package called PrintMe
 
