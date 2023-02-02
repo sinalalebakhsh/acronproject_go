59a5951e390ebf2ebdf2fe4a21c0830728a16965
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-02 11:27:03 -0500

    Imp main.go

--- main.go
+++ main.go
@@ -2,36 +2,18 @@ package main
 
 import (
 	"fmt"
-	"os"
-	// "myapp/game"
+	"myapp/game"
 )
 
 func main() {
-	// playAgain := true
+	playAgain := true
 
-	// for playAgain {
-	// 	// game.Play()
-	// 	playAgain = game.GetYesOrNo("Would you like to play again (y/n)?")
-	// }
-
-	// fmt.Println("")
-	// fmt.Println("Goodbye.")
-
-	fmt.Println("Home:", os.Getenv("HOME"))
-	// fmt.Println("Shell:", os.Getenv("SHELL"))
-
-	shell, ok := os.LookupEnv("SHELL")
-	if !ok  {
-		fmt.Printf("The Env var SHELL is not set")
-	} else  {
-		fmt.Println("Shell:", shell)
+	for playAgain {
+		// game.Play()
+		playAgain = game.GetYesOrNo("Would you like to play again (y/n)?")
 	}
 
-	err := os.Setenv("NAME", "Sina")
-	if err != nil  {
-		fmt.Printf("Could not set the env var NAME")
-	} else {
-		fmt.Printf("NAME: %s\n", os.Getenv("NAME"))
-	}
+	fmt.Println("")
+	fmt.Println("Goodbye.")
 
 }
