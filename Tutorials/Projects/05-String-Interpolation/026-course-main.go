0d7b0559af3a5f29583ace979730aca0a07a9c17
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-03 05:08:46 -0500

    Create Archive 026 course

---
+++ Tutorials/Projects/05-String-Interpolation/026-course-main.go
@@ -0,0 +1,57 @@
+// String Interpolation 026 number course
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var reader *bufio.Reader
+
+func main()  {
+	reader = bufio.NewReader(os.Stdin)
+	userInput := readSting("What's your name?")
+	age := readInt("How old are you?")
+
+	fmt.Println(fmt.Sprintf("Your name's %s, and you'r %d years old.", userInput, age))
+}
+
+func prompt()  {
+	fmt.Print("->  ")
+}
+
+func readSting(a string) string  {	
+	for  {
+		fmt.Println(a)
+		prompt()
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\n", "", -1)
+		if userInput != ""  {
+			return userInput
+		} else  {
+			fmt.Println("Please enter name with a value")
+		}
+	}
+}
+
+
+func readInt(a string) int  {
+	for  {
+		fmt.Println(a)
+		prompt()
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\n", "", -1)
+	
+		number, err := strconv.Atoi(userInput)
+		if err != nil  {
+			fmt.Println("Please enter a whole number")
+		} else  {
+			return number
+		}
+	}
+	
+}
+
