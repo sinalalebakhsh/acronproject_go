5c8e889fc60cd35a400f784d32de5134348d0c75
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-03 03:30:34 -0500

    Imp main.go

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func prompt()  {
 func readSting(a string) string  {	
 
 	fmt.Println(a)
-
+	prompt()
 
 	userInput, _ := reader.ReadString('\n')
 	userInput = strings.Replace(userInput, "\n", "", -1)
