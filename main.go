84308e15d2fe43df519bc40ffc73044f4f1ba1cb
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-05 14:29:49 -0500

    Imp main.go

--- main.go
+++ main.go
@@ -17,22 +17,5 @@ var myFloat64 float64
 // interface type
 
 func main() {
-	myInt = 10
-	myUint = 20
-
-	myFloat = 10.1
-	myFloat64 = 100.1
-
-	log.Println(myInt, myUint, myFloat, myFloat64)
-
-	myString := "Sina"
-	log.Println(myString)
-	myString = "Kimia"
-
-
-
-	var myBool = true
-	myBool = false
-
-	log.Println(myBool)
+	
 }
