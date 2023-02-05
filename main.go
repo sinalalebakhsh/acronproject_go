b0c3a87e9a02cc6d836730fb32a81cc83e31c2e5
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-05 13:23:23 -0500

    Imp main.go

--- main.go
+++ main.go
@@ -2,4 +2,14 @@
 
 package main
 
-// basic types ()
+// basic types (numbers, string, booleans)
+
+// aggregate types (array, struct)
+
+// reference types (pointers, silence, maps, functions, channels)
+
+// interface type
+
+func main() {
+
+}
