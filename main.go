042bf8a8d5dabdd3a158fa70b4dfbf879979ed19
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-05 15:17:59 -0500

    Imp main.go Memory Location

--- main.go
+++ main.go
@@ -14,4 +14,14 @@ func main() {
 	fmt.Println("x is", x)
 	fmt.Println("myFirstPointer is to Memory Location:", myFirstPointer)
 
+	*myFirstPointer = 15
+	fmt.Println("x is now", x)
+
+	changeValueOfPointer(&x)
+	fmt.Println("After function call, x is now", x)
+}
+
+
+func changeValueOfPointer(num *int)  {
+	*num = 25
 }
