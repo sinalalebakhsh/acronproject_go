3198443d22e719b0ef38a1e8bc268df6c27be9ce
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-05 15:03:05 -0500

    Imp main.go

--- main.go
+++ main.go
@@ -4,37 +4,13 @@ package main
 
 import "fmt"
 
-// basic types (numbers, string, booleans)
-
-// aggregate types (array, struct)
-
-type Car struct {
-	NumberOfTires int
-	Luxery        bool
-	BucketSeats   bool
-	Make          string
-	Model         string
-	Year          int
-}
-
 // reference types (pointers, silence, maps, functions, channels)
 
 // interface type
 
 func main() {
-	// var myCar Car
-	// myCar.NumberOfTires = 4
-	// myCar.Luxery = true
-	// myCar.Make = "mercedes-benz-unimog"
+	var myInt int
+	myInt = 10
 
-	// ---------------------------------------- Another Definition
-	myCar1 := Car{
-		NumberOfTires: 4,
-		Luxery:        true,
-		BucketSeats:   true,
-		Make:          "mercedes-benz-unimog",
-		Year:          2030,
-	}
-
-	fmt.Println(myCar1)
+	fmt.Println(myInt)
 }
