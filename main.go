5a7f1e5edff9aa263ac5cc23d072ef82cfab3f89
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-06 05:07:44 -0500

    Imp main.go

--- main.go
+++ main.go
@@ -2,7 +2,7 @@
 
 package main
 
-
+import "fmt"
 
 // reference types (pointers, silence, maps, functions, channels)
 
@@ -10,12 +10,16 @@ package main
 
 func main() {
 	
-	var myMap map[string]string
+	// var myMap map[string]string
 
 	intMap := make(map[string]int)
 
 	intMap["one"] = 1
 	intMap["two"] = 2
 	intMap["three"] = 3
+
+	for key, value := range intMap  {
+		fmt.Println(key, value)
+	}
 }
 
