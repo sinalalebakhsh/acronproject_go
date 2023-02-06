519c82be2d6e4328c2dee0107254d7ce1303f7f6
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-06 11:48:38 -0500

    Imp main.go

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
-//039 Channels
+//040 Interface
 
 package main
 
 /*
-reference types (pointers, silence, maps, functions, channels)
+Interfaces type
 -------------------------------
 
 */
 
+// Dog id the type for dogs
+type Dog struct {
+	Name         string
+	Sound        string
+	NumberOfLegs int
+}
+
+// Cat is the type for cats
+type Cat struct {
+	Name         string
+	Sound        string
+	NumberOfLegs int
+	HasTail      bool
+}
+
 func main() {
+	// ask a riddle !!! یک معما بپرس
 }
