6801ef3ba325ecdbbb7d8bc810f9637eedb6bc34
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-06 10:41:17 -0500

    Delete unuseful codes

--- main.go
+++ main.go
@@ -2,32 +2,12 @@
 
 package main
 
-import "fmt"
-
 // reference types (pointers, silence, maps, functions, channels)
 
 // interface type
 
-// RESULT => First concurrently ------
 // Go routine
 
 func main() {
-	go doSomeThing("Hello, World")
-
-	fmt.Println("This is another message")
-	for  {
-		// do nothing
-	}
+	
 }
-
-func doSomeThing(s string)  {
-	until := 0
-	for  {
-		fmt.Println("s is", s)
-		until = until + 1
-		if until == 6 {
-			break
-		}
-	}
-}
-
