15c461ced8316f0a863bcc86c67f744ba6fb5566
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-06 05:39:30 -0500

    Imp main.go For change value of an index Map

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import "fmt"
 // interface type
 
 func main() {
-	
+
 	// var myMap map[string]string
 
 	intMap := make(map[string]int)
@@ -22,24 +22,29 @@ func main() {
 
 	// In the Go language, reading by the loop is done randomly.
 	// This behavior is weird !!!
-	for key, value := range intMap  {
+	for key, value := range intMap {
 		fmt.Println(key, value)
 	}
-
 
 	fmt.Println("--------------")
 	// For delete an element in Map
 	// If comment it below first code
 	// delete(intMap, "five")
-	
 
 	element, ok := intMap["five"]
-	if ok  {
+	if ok {
 		fmt.Println(element, "exsit In intMap.")
 	} else {
 		fmt.Println(element, "exsit not In intMap.")
 	}
 
+	// For change value of an index Map
+	intMap["five"] = 333
+	element2, ok := intMap["five"]
+	if ok {
+		fmt.Println(element2, "exsit In intMap.")
+	} else {
+		fmt.Println(element2, "exsit not In intMap.")
+	}
 
 }
-
