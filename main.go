ca08fddcaaaa8a3279547e1f8f59b702fdc27ec8
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-06 06:14:58 -0500

    For sum infinite numbers

--- main.go
+++ main.go
@@ -2,10 +2,29 @@
 
 package main
 
+import "fmt"
+
 // reference types (pointers, silence, maps, functions, channels)
 
 // interface type
 
 func main() {
 
+	mySumNumbers := sumMany(1,1,2,30)
+
+	fmt.Println(mySumNumbers)
+
 }
+
+// For sum infinite numbers:
+func sumMany(nums ...int) int  {
+	total := 0
+
+	for _, x := range nums  {
+		total = total + x
+	}
+
+	return total
+}
+
+
