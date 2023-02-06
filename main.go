1bca39d6d884ce760fc75afb977f74127f52473e
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-06 06:10:19 -0500

    Delete unuseful code

--- main.go
+++ main.go
@@ -2,29 +2,10 @@
 
 package main
 
-import "fmt"
-
 // reference types (pointers, silence, maps, functions, channels)
 
 // interface type
 
 func main() {
-	z := addTwoNumber(2,3)
-	fmt.Println(z)
+
 }
-
-// func addTwoNumber(x, y int) int {
-// 	return x + y
-// }
-
-//Also you can write like it:
-// But it's bad readable for another programmer
-func addTwoNumber(x, y int) (sum int) {
-	sum = x + y
-	return
-}
-
-
-
-
-
