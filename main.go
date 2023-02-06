6ec78f74a1d09142b50b178ef44b95dbfa6a862c
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-06 06:04:37 -0500

    Function Definition

--- main.go
+++ main.go
@@ -2,10 +2,17 @@
 
 package main
 
+import "fmt"
+
 // reference types (pointers, silence, maps, functions, channels)
 
 // interface type
 
 func main() {
+	z := addTwoNumber(2,3)
+	fmt.Println(z)
+}
 
+func addTwoNumber(x, y int) int {
+	return x + y
 }
