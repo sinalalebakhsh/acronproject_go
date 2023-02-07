6078baebcd2b5f9c8b8589750f2a8a1b020b00df
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-07 07:36:50 -0500

    Imp main.go

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 //047-Exported-VS-Unexported.go
 package main
 
+import "acron/staff"
+
 func main() {
 
+}
+
+var employees = []staff.Employee {
+	{FirstName: "Sina", LastName: "Lalebakhsh", Salary: 900000000, FullTime: true,},
+	{FirstName: "Gandom", LastName: "Irani", Salary: 123000000, FullTime: false,},
+	{FirstName: "Kimia", LastName: "Tehrani", Salary: 4302000000, FullTime: false,},
+	{FirstName: "Mahsa", LastName: "Gilani", Salary: 999999999999, FullTime: false,},
 }
