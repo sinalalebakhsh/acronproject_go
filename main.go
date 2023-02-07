d5974e101a2fed3be2848624923f06ea2851f68c
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-07 09:36:18 -0500

    Imp main.go

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 //047-Exported-VS-Unexported.go
 package main
 
-import "acron/staff"
+import (
+	"acron/staff"
+	"fmt"
+)
 
 func main() {
-
+	myStaff := staff.Office {
+		AllStaff: employees,
+	}
+	// fmt.Println(myStaff.All())
+	fmt.Println(myStaff.Overpaid())
 }
 
 var employees = []staff.Employee {
-	{FirstName: "Sina", LastName: "Lalebakhsh", Salary: 900000000, FullTime: true,},
-	{FirstName: "Gandom", LastName: "Irani", Salary: 123000000, FullTime: false,},
-	{FirstName: "Kimia", LastName: "Tehrani", Salary: 4302000000, FullTime: false,},
-	{FirstName: "Mahsa", LastName: "Gilani", Salary: 999999999999, FullTime: false,},
+	{FirstName: "Sina", LastName: "Lalebakhsh", Salary: 400, FullTime: true,},
+	{FirstName: "Gandom", LastName: "Irani", Salary: 1233, FullTime: false,},
+	{FirstName: "Kimia", LastName: "Tehrani", Salary: 12345, FullTime: false,},
+	{FirstName: "Mahsa", LastName: "Gilani", Salary: 7543211, FullTime: false,},
 }
