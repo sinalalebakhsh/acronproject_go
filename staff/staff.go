5481dcdc821ed29b26f51a9edcc0b15918aca369
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-07 09:36:42 -0500

    Imp staff.go

--- staff/staff.go
+++ staff/staff.go
@@ -10,3 +10,18 @@ type Employee struct {
 type Office struct {
 	AllStaff []Employee
 }
+
+func (e *Office) All() []Employee {
+	return e.AllStaff
+}
+
+func (e *Office) Overpaid() []Employee {
+	var overpaid []Employee
+
+	for _, x := range e.AllStaff {
+		if x.Salary > 75000 {
+			overpaid = append(overpaid, x)
+		}
+	}
+	return overpaid
+}
