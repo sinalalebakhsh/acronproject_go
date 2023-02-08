aaa46b1871aa520c713cf7b43b055baff80237dc
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-08 00:37:58 -0500

    Imp main.go

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 // 052-The-While-Loop.go
 package main
 
+import (
+	"math/rand"
+	"time"
+)
+
 
 func main() {
+	// get a random number between 1 to 1001
+	rand.Seed(time.Now().UnixNano())
 	i := 1000
 	for i > 100 {
 		// this is infinite loop
