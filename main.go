8f3a75f4cc6cebc8a9264f4a40587d297def67aa
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-08 11:52:02 -0500

    Ready for 054

--- main.go
+++ main.go
@@ -1,28 +1,5 @@
-// 053-The-Infinite-Loop.go
+// 054-Nested-Loops-and-the-Debugger.go
 package main
 
-import (
-	"acron/mylogger"
-	"bufio"
-	"fmt"
-	"os"
-	"time"
-)
-
-
 func main() {
-	// read input from the user 5 times and write it to a log
-	reader := bufio.NewReader(os.Stdin)
-	ch := make(chan string)			
-
-	// Get log from user,  concurrent program
-	go mylogger.ListenForLog(ch)
-
-	fmt.Println("Enter something:")
-	for i:= 0; i < 5; i++ {
-		fmt.Print("-> ")
-		input, _ := reader.ReadString('\n')
-		ch <- input
-		time.Sleep(time.Second)
-	}
 }
