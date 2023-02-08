6d938cf557411c8abd053952e54a3ee6d9fa28bf
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-08 00:43:44 -0500

    move to archive

--- Tutorials/Projects/07-Flow-Control/052-The-While-Loop.go
+++ Tutorials/Projects/07-Flow-Control/052-The-While-Loop.go
@@ -1,1 +1,25 @@
 // 052-The-While-Loop.go
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+
+func main() {
+	// get a random number between 1 to 1001
+	rand.Seed(time.Now().UnixNano())
+
+	i := 1000
+	for i > 100 {
+		// this is infinite loop
+		i = rand.Intn(1000) + 1
+		fmt.Println("i:",i)
+		if i > 100 {
+			fmt.Println("i:", i, "loop keeps going..")
+		}
+	}
+	fmt.Println("Got", i, "and broke out of loop [**]")
+}
