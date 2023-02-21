cd3fc9a37126bf9c76515290356d578ea4267112
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-21 12:51:49 -0500

    Delete All

--- go.mod
+++
@@ -1,5 +0,0 @@
-module acron
-
-go 1.18
-
-require golang.org/x/tour v0.1.0 // indirect
--- main.go
+++ main.go
@@ -1,37 +1,3 @@
 // Source : A Tour of Go 
 
-package main
-
-import (
-	// "fmt"
-	"strings"
-	"golang.org/x/tour/wc"
-)
-
-func main() {
-	// myMap := WordCount("sina lale bakhsh")
-	// fmt.Println(myMap)
-	wc.Test(WordCount)
-
-}
-
-
-
-// package main
-
-func WordCount(s string) map[string]int {
-	myMap := map[string]int{}
-
-	vari := strings.Fields(s)
-	for i := range vari {
-		myMap[vari[i]] = len(vari[i])  
-		
-	}
-	
-	return myMap
-}
-
-// func main() {
-// 	wc.Test(WordCount)
-// }
 
