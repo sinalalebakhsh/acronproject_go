deb88c0941229f7434fecffb4786d86db83b809b
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-24 05:30:36 -0500

    Create new project about: knowlege user about capital of countries.

---
+++ con_cap/con_cap.go
@@ -0,0 +1,32 @@
+package con_cap
+
+
+var CountriesCapital = map[string]string{
+	"AFGHANISTAN": "KABUL",
+	"ALBANIA": "TIRANA",
+	"ALGERIA": "ALGIERS",
+	"ANDORRA": "ANDORRA LA VELLA",
+	"ANGOLA": "LUANDA",
+	"ANTIGUA & BARBUDA": "SAINT JOHN'S",
+	"ARGENTINA": "BUENOS AIRES",
+	"ARMENIA": "YEREVAN",
+	"AUSTRALIA": "CANBERRA",
+	"AUSTRIA": "VIENNA",
+	"AZERBAIJAN": "BAKU",
+	// -------------------------------- 
+	"afghanistan": "kabul",
+	"albania": "tirana",
+	"algeria": "algiers",
+	"andorra": "andorra la vella",
+	"angola": "luanda",
+	"antigua & barbuda": "saint john's",
+	"argentina": "buenos aires",
+	"armenia": "yerevan",
+	"australia": "canberra",
+	"austria": "vienna",
+	"azerbaijan": "baku",
+}
+
+
+
+
--- main.go
+++ main.go
@@ -1,3 +1,19 @@
-// Source : A Tour of Go 
+// Source: https://www.tutorialspoint.com/how-to-get-input-from-the-user-in-golang
+
+package main
+
+import (
+	"fmt"
+	"acron/con_cap"
+)
+
+
+func main() {
+	for key, value := range con_cap.CountriesCapital {
+		fmt.Println(key, "	:	", value)
+	}
+
+}
+
 
 
